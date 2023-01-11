7d393903b68c817c3dd69b43b92a4c7fbfc18132
mraogudi <malligmrao514@gmail.com>
2023-01-11 12:47:30 +0530

    Component changes

--- src/app/book.service.ts
+++ src/app/book.service.ts
@@ -10,7 +10,7 @@ import { Observable } from 'rxjs';
 export class BookService {
   bookStoreUrl: string = 'http://localhost:8082/bookstore/';
   addUrl: string = this.bookStoreUrl+'add';
-  updateUrl: string = this.bookStoreUrl+'update'
+  updateUrl: string = this.bookStoreUrl+'update/'
   deleteUrl: string = this.bookStoreUrl+'delete/'
   getUrl: string = this.bookStoreUrl+'get'
   getAllUrl: string = this.bookStoreUrl+'all'
@@ -27,8 +27,8 @@ export class BookService {
   }
 
   updateBook(name: any, price: any, author: any, desc: any, id: any): Observable<Object> {
-    const body = {'id':id, 'name':name, 'author':author, 'price': price, 'shortDesc': desc}
-    return this.http.put(this.updateUrl, body);
+    const body = {'name':name, 'author':author, 'price': price, 'shortDesc': desc}
+    return this.http.put(this.updateUrl+id, body);
   }
 
   getAllBooks(): Observable<Books[]> {
